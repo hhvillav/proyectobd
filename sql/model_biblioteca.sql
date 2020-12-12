803a1410b45128cae94c06c8b0de87d08ea06fe3
= <hhvilla@gmail.com>
2020-12-11 23:37:17 -0500

    Se ajustan relaciones

--- sql/model_biblioteca.sql
+++ sql/model_biblioteca.sql
@@ -1,4 +1,4 @@
-CREATE TABLE public.autores (
+CREATE TABLE autores (
 	id_a int4 NOT NULL,
 	nom_autor varchar(50) NULL,
 	nacionalidad varchar(50) NULL DEFAULT 0,
@@ -6,15 +6,40 @@ CREATE TABLE public.autores (
 );
 
 
-CREATE TABLE public.ejemplares (
+CREATE TABLE ejemplares (
 	isbn numeric(14) NOT NULL,
 	num_ej int4 NOT NULL
 );
 
 
-CREATE TABLE public.escribe (
-	isbn int4 NOT NULL,
+CREATE TABLE escribe (
+	isbn numeric NOT NULL,
 	id_a int4 NOT NULL,
 	CONSTRAINT escribe_pk PRIMARY KEY (isbn, id_a)
 );
 
+-- public.escribe foreign keys
+ALTER TABLE public.escribe ADD CONSTRAINT escribe_fk_autores FOREIGN KEY (id_a) REFERENCES autores(id_a);
+ALTER TABLE public.escribe ADD CONSTRAINT escribe_fk_libros FOREIGN KEY (isbn) REFERENCES libros(isbn);
+
+CREATE TABLE presta (
+	cod_e numeric NOT NULL,
+	isbn numeric(14) NOT NULL,
+	num_ej int4 NOT NULL,
+	fech_p timestamp NOT NULL,
+	fech_d timestamp NULL,
+	CONSTRAINT presta_pk PRIMARY KEY (cod_e, isbn, num_ej, fech_p)
+);
+CREATE TABLE referencia (
+	cod_a int4 NOT NULL,
+	isbn numeric(14) NOT NULL,
+	CONSTRAINT referencia_pk PRIMARY KEY (cod_a, isbn)
+);
+
+CREATE TABLE public.libros (
+	isbn numeric NOT NULL,
+	titulo varchar NOT NULL,
+	edicion int4 NOT NULL,
+	editorial varchar NOT NULL,
+	CONSTRAINT libros_pk PRIMARY KEY (isbn)
+);
--- sql/proc_bibliotecario.sql
+++ sql/proc_bibliotecario.sql
@@ -1,19 +1,54 @@
+-- ------------------------------------------------------------------------------
+-- Administración de conexiones
+-- ------------------------------------------------------------------------------
+-- Mantiene las conexiones en una vista para facilitar su matenimiento
+CREATE OR REPLACE VIEW public.lista_bd_facultades
+AS SELECT 'facultad_artes'AS nombre,
+    'localhost' AS servidor,
+    '5432' AS puerto
+UNION
+ SELECT 'facultad_ciencias_y_educacion'::text AS nombre,
+    'localhost' AS servidor,
+    '5432' AS puerto
+UNION
+ SELECT 'facultad_ingenieria'::text AS nombre,
+    'localhost' AS servidor,
+    '5432' AS puerto
+UNION
+ SELECT 'facultad_tecnologica'::text AS nombre,
+    'localhost' AS servidor,
+    '5432' AS puerto
+UNION
+ SELECT 'facultad_medio_ambiente'::text AS nombre,
+    'localhost' AS servidor,
+    '5432' AS puerto;
 
--- Retorna la carrera de un estudiante,sirev para validar que el estudiante se encuebtre activo
+
+
+-- ------------------------------------------------------------------------------
+-- Administración de prestamos
+-- ------------------------------------------------------------------------------
+
+-- Retorna la carrera de un estudiante. Validar que el estudiante se encuentre activo
 
 CREATE OR REPLACE FUNCTION carrera_estudiante(codigo_estudiante integer)
   RETURNS integer AS
 $carrera_estudiante$
 declare c int;
 declare consulta char(70);
+declare reg_fac RECORD;
 begin
+	consulta := concat('select id_carr from estudiantes where cod_e =',codigo_estudiante);
     -- Hay que repetir este if por cada facultad
-	consulta := concat('select id_carr from estudiantes where cod_e =',codigo_estudiante); 
-	select * from dblink('dbname=universidad user=postgres password=sa',consulta) 
+	for reg_fac in select	* from	lista_bd_facultades loop
+	-- consulta por cada facultad
+	select * from dblink(concat('dbname=', reg_fac.nombre, ' host=', reg_fac.servidor_bd, ' user=postgres password=sa'), consulta)
 	as (carr int)into c;
        RETURN c;
-END
-$carrera_estudiante$ LANGUAGE plpgsql;	
+	-- fin consulta por cada facultad
+	end loop;
+end $carrera_estudiante$ language plpgsql;
+
 
 -- Verica un préstamo de lbros en la biblioteca,valida que el estudiante esté
 -- inscrito en una carrera
@@ -21,12 +56,12 @@ CREATE OR REPLACE FUNCTION verificar_prestamo()
  RETURNS trigger
  LANGUAGE plpgsql
 AS $function$
-declare 
-begin 
+declare
+begin
 	if (TG_OP = 'INSERT' ) then
 		if carrera_estudiante(new.cod_e) is not null then
 			return new;
-		else 
+		else
 			return null;
 			end if;
 	end if;
@@ -35,28 +70,118 @@ $function$;
 
 -- Impide que se registre un préstamo para un estudiante no registrado
 create trigger verificar_prestamo before insert or update on presta for each row execute procedure verificar_prestamo();
- 
+
 
 -- Inserta un préstamo en la base de datos
 create or replace function insertar_prestamo(cod_estudiante int, isbn_p numeric, num_ej_p int )
 returns void as $insertar_prestamo$
-declare 
-begin 
+declare
+begin
 	insert into presta(cod_e,isbn,num_ej, fech_p) values (cod_estudiante,isbn_p, num_ej_p, now());
 end
 $insertar_prestamo$ language plpgsql;
 
--- Retorna  un préstamo en la base de datos 
+-- Retorna  un préstamo en la base de datos
 create or replace function retornar_prestamo(cod_estudiante int, isbn_p numeric, num_ej_p int )
 returns void as $verificar_estudiante$
-declare 
-begin 
+declare
+begin
 	update  presta set
 		fech_d = now()
-	where 
+	where
 		cod_e = cod_estudiante and
 		isbn = isbn_p and
 		num_ej = num_ej_p and
 		fech_d is null;
 end
 $verificar_estudiante$ language plpgsql;
+
+-- ------------------------------------------------------------------------------
+-- Administración de ejemplares
+-- ------------------------------------------------------------------------------
+-- Inserta un ejemplar en la base de datos
+create or replace function insertar_ejemplar(isbn_n numeric, num_ej_n int )
+returns void as $insertar_ejemplar$
+declare
+begin
+	insert into ejemplar (isbn,num_ej) values (isbn_p_n, num_ej_p_n);
+end
+$insertar_ejemplar$ language plpgsql;
+
+
+-- Elimina un ejemplar de la base de datos
+create or replace function eliminar_ejemplar(isbn_e numeric, num_ej_e int )
+returns void as $eliminar_ejemplar$
+declare
+begin
+	delete from ejemplar where isbn = isbn_e and num_ej = num_ej_e;
+end
+$eliminar_ejemplar$ language plpgsql;
+
+-- ------------------------------------------------------------------------------
+-- Administración de libros
+-- ------------------------------------------------------------------------------
+create or replace function insertar_libro(isbn_n numeric,titulo_n varchar,edicion_n int,editorial_n varchar)
+returns void as $insertar_libro$
+declare
+begin
+	insert into libros (isbn ,titulo ,edicion ,editorial ) values (isbn_n ,titulo_n,edicion_n,editorial_n);
+end
+$insertar_libro$ language plpgsql;
+
+-- Actualiza libro
+create or replace function actualizar_libro(isbn_n numeric,titulo_n varchar,edicion_n int,editorial_n varchar)
+returns void as $actualizar_libro$
+declare
+begin
+	update  libros set
+		titulo = titulo_n,
+		edicion = edicion_n,
+		editorial = editorial_n
+	 where
+		isbn = isbn_n;
+end
+$actualizar_libro$ language plpgsql;
+
+-- Elimina un libro de la base de datos
+create or replace function eliminar_libro(isbn_e numeric)
+returns void as $eliminar_libro$
+declare
+begin
+	delete from libros where isbn = isbn_e;
+end
+$eliminar_libro$ language plpgsql;
+
+-- ------------------------------------------------------------------------------
+-- Administración de autores
+-- ------------------------------------------------------------------------------
+
+create or replace function insertar_autor(id_a_n integer, nom_autor_n varchar, nacionalidad_n varchar)
+returns void as $insertar_autor$
+declare
+begin
+	insert into autores (id_a, nom_autor, nacionalidad ) values (id_a_n , nom_autor_n , nacionalidad_n);
+end
+$insertar_autor$ language plpgsql;
+
+-- Actualiza libro
+create or replace function actualizar_autor(id_a_n integer, nom_autor_n varchar, nacionalidad_n varchar)
+returns void as $actualizar_autor$
+declare
+begin
+	update  autores set
+		nom_autor_n = nom_autor_n,
+		nacionalidad = nacionalidad_n
+	 where
+		id_a = id_a_n;
+end
+$actualizar_autor$ language plpgsql;
+
+-- Elimina un libro de la base de datos
+create or replace function eliminar_autor(id_a_n integer)
+returns void as $eliminar_autor$
+declare
+begin
+	delete from autores where id_a = id_a_n;
+end
+$eliminar_autor$ language plpgsql;
